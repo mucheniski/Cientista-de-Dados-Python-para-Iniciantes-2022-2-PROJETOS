23943601c0e9786cbe00f0d64c51a4cca15bdb05
Diego Mucheniski <mucheniski@gmail.com>
2022-01-27 19:21:48 -0300

    19. Filtrando dados com condições

--- Secao5_PandasMod/MelbourneHousesPrice.py
+++ Secao5_PandasMod/MelbourneHousesPrice.py
@@ -3,4 +3,13 @@ import pandas as pd
 # df = DataFrame
 df = pd.read_csv('../datasets/house.csv')
 
-print(df)
+# print(df)
+
+# Apenas casas com 3 quartos que estejam abaixo do valor 300000
+exemplo1 = df.loc[
+                  (df['Type'] == 'h')
+                & (df['Rooms'] == 3)
+                & (df['Price'] <= 300000)
+            ]
+
+print(exemplo1)
