eea28caf2d640fe493df55a0c713732bb86b9247
Diego Mucheniski <mucheniski@gmail.com>
2022-01-29 14:43:24 -0300

    32. Importando o Dataset

---
+++ Secao7_MatPlotlib/PrimeiroGrafico.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Importando o csv para um dataframe com pandas
+gasdf = pd.read_csv('../datasets/gas_prices.csv')
+print(gasdf)
+
