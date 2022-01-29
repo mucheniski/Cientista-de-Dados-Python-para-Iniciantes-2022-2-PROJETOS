016110c82d18ec7e20990920a8b095ddc14f295d
Diego Mucheniski <mucheniski@gmail.com>
2022-01-29 18:11:06 -0300

    icecream - exportando grafico

--- Projeto2_IceCream/IceCreamDiego.py
+++ Projeto2_IceCream/IceCreamDiego.py
@@ -5,7 +5,17 @@ iceCreamDf = pd.read_csv('../datasets/icecreamsales.csv')
 
 plt.plot(
     iceCreamDf['Temperature'],
-    iceCreamDf['Sales']
+    iceCreamDf['Sales'],
+    'r.--',
+    label='US$'
 )
+
+plt.title('Ice Cream Truck')
+plt.xlabel('Temperature')
+plt.ylabel('Sales (US$)')
+
+plt.legend()
+
+plt.savefig('../grafics/iceCreamDiego.pdf')
 
 plt.show()
