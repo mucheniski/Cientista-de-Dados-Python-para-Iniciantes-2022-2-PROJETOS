7217ef4dd0dce991c768c60e24917825d8ee63a0
Diego Mucheniski <mucheniski@gmail.com>
2022-01-29 16:34:05 -0300

    titanic - filtrando as colunas

--- Projeto1_Titanic/TitanicDiego.py
+++ Projeto1_Titanic/TitanicDiego.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
+# lendo o arquivo
 titanicDf = pd.read_csv('../datasets/titanic.csv')
+
+# Buscando apenas as colunas necessárias Colunas - Name | Age | Sex | Pclass | Survived
+titanicDf = titanicDf[['Name', 'Age', 'Sex', 'Pclass', 'Survived']]
 print(titanicDf)
