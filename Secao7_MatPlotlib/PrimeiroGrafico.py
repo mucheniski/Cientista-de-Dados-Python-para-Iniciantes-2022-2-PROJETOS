8b2ea4418d3f83ab20afcc9b0420025ad9ccfe4c
Diego Mucheniski <mucheniski@gmail.com>
2022-01-29 14:59:27 -0300

    34. Adicionando Legenda e Ticks

--- Secao7_MatPlotlib/PrimeiroGrafico.py
+++ Secao7_MatPlotlib/PrimeiroGrafico.py
@@ -4,10 +4,23 @@ import matplotlib.pyplot as plt
 
 # Importando o csv para um dataframe com pandas
 gasdf = pd.read_csv('../datasets/gas_prices.csv')
-print(gasdf)
+# print(gasdf)
 
 # Criando um gráfico de linhas comparando preços entre 3 países eixos x, y
-plt.plot(gasdf['Year'], gasdf['Australia'])
-plt.plot(gasdf['Year'], gasdf['Italy'])
-plt.plot(gasdf['Year'], gasdf['USA'])
+
+# Adicionando título
+plt.title("Valores de combustível em US$")
+
+# Adicionando valores (eixo X, eixo Y)
+plt.plot(gasdf['Year'], gasdf['Australia'], label='Australia')
+plt.plot(gasdf['Year'], gasdf['Italy'], label='Italy')
+plt.plot(gasdf['Year'], gasdf['USA'], label='USA')
+plt.plot(gasdf['Year'], gasdf['Japan'], label='Japan')
+
+# Formatando o valor dos anos no eixo X
+# [::2] do início ao fim apresentando de 2 em 2 anos
+plt.xticks(gasdf['Year'][::2])
+
+# Adicoinando a legenda e apresentando o gráfico
+plt.legend()
 plt.show()
